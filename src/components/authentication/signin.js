aaecdd964ca59127c2568a374db70d6704668cef
Richard Hertzog <richard.hertzog@gmail.com>
2017-03-09 12:11:04 -0800

    complete signin component design

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -7,18 +7,30 @@ export default class Signin extends Component {
       username: '',
       password: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    const { value, name } = event.target;
+    const state = {};
+    state[name] = value;
+    this.setState(state);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ username: '', password: '' });
   }
 
   render() {
     return (
-      <form className="signin" action="">
+      <form className="signin" action="" onSubmit={this.handleSubmit}>
         <fieldset className="form-group">
           <label>Username:</label>
-          <input name="username" className="form-control" require />
+          <input name="username" value={this.state.username} onChange={this.handleChange} className="form-control" required />
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input name="password" className="form-control" require />
+          <input name="password" value={this.state.password} onChange={this.handleChange} className="form-control" required />
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign In</button>
       </form>
