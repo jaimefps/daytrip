c724fc52d9e10d31107c3df8e801aa27c3c59cb5
Aloak <aloak92@gmail.com>
2017-03-08 22:17:32 -0800

    fixes spacing issue in code

--- server/services/passport.js
+++ server/services/passport.js
@@ -10,26 +10,25 @@ const jwtOptions = {
 	secretOrKey: config.secret 
 };
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
-	User.findById(payload.sub).then(user => {
-		if (user) {
-			done(null, user);
-		} else {
-			done(null, false)
-		}
-	}, e => done(e, false))
+  User.findById(payload.sub).then(user => {
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false)
+    }
+  }, e => done(e, false))
 })
 
 const localOptions = { usernameField: 'username' }
 const localLogin = new LocalStrategy(localOptions, function(username, password, done) {
-	User.findOne({username}).then(user => {
-		if (!user) return done(null, false);
-		user.comparePassword(password, (err, match) => {
-			if (err) return done(err);
-			if(!match) return done(null, false);
-
-			return done(null, user);
-		})
-	})
+  User.findOne({username}).then(user => {
+    if (!user) return done(null, false);
+    user.comparePassword(password, (err, match) => {
+      if (err) return done(err);
+      if(!match) return done(null, false);
+      return done(null, user);
+    })
+  })
 }, e => done(e))
 
 passport.use(jwtLogin)
