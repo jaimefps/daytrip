4c9492ac6ce3f4bed2fa97646f6b8cb15ad08e30
Aloak <aloak92@gmail.com>
2017-03-09 11:38:31 -0800

    adds basic components and links bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import Header from './header/header'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      authenticated: false
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        {this.props.children}
       </div>
     );
   }
