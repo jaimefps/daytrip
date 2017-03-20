cf07875cc5d47e9aa1250248513dbbe1a506c006
Aloak <aloak92@gmail.com>
2017-03-20 00:57:11 -0700

    add essential bug fixes and new map marker

--- src/components/header/dropdown.js
+++ src/components/header/dropdown.js
@@ -9,10 +9,6 @@ export default class Dropdown extends Component {
         <ul className="dropdown-menu">
           <li><Link to={`/profile/${this.props.username}`}>My Profile</Link></li>
           <li><Link to="createtrip">Create Trip</Link></li>
-          <li><a href="#">Something else here</a></li>
-          <li role="separator" className="divider"></li>
-          <li className="dropdown-header">Nav header</li>
-          <li><a href="#">Separated link</a></li>
           <li role="separator" className="divider"></li>
           <li><Link to="/signout">Sign Out</Link></li>
         </ul>
