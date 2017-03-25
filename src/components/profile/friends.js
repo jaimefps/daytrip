59f1f82a61d3e59c75a7d01cd64d6c70ca572118
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-03-25 12:03:35 -0700

    correctly rendering button for add/remove friend in Tiles

--- src/components/profile/friends.js
+++ src/components/profile/friends.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 import axios from 'axios';
 import config from '../../config';
-
+import _ from 'lodash';
 
 export default class Friends extends Component {
   constructor(props) {
@@ -10,17 +10,47 @@ export default class Friends extends Component {
     this.state = {
       rank: 0,
       created: 0,
+      userInfo: { 
+        friends: []
+      }
     };
   }
 
   componentDidMount() {
     this.fetchTrips();
+    this.getUserInfo();
   }
 
   handleClick() {
     const userLink = `/profile/${this.props.friend}`;
     browserHistory.replace(userLink);
   }
+
+  // jaime START
+  getUserInfo() {
+    return axios.get(`${config.server}/user`, {
+      params: {
+        username: localStorage.getItem('username'),
+      },
+      headers: {
+        authorization: localStorage.getItem('token'),
+      },
+    })
+    .then((res) => {
+      this.setState({ userInfo: res.data });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  }
+
+  renderFriendsButtonCaption() {
+    return _.includes(this.state.userInfo.friends, this.props.friend) ? 'Remove friend' : 'Add friend';
+  }
+  renderAddFriend() {
+      return(<button style={{ float: "right" }}>{ this.renderFriendsButtonCaption() }</button>)
+  }
+  // jaime END
 
   fetchTrips() {
     return axios.get(`${config.server}/trips`, {
@@ -33,8 +63,9 @@ export default class Friends extends Component {
     })
     .then(res => this.setState({ rank: res.data.reduce((a,b) => a + b.likes, 0), created: res.data.length }))
   }
-
   render() {
+    console.log('friends.js PROPS`', this.props)
+    console.log('friends.js STATE', this.state)
     return (
       <div className="panel panel-default">
         <div className="panel-body">
@@ -44,6 +75,7 @@ export default class Friends extends Component {
             <h4> Rank: {this.state.rank} </h4>
             <h4> Trips Created: {this.state.created} </h4>
           </div>
+          {this.renderAddFriend()}
         </div>
       </div>
     );
